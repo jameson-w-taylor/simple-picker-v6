48f6862a835a5853bc98ebfd90700f48d334e32f
Jameson Taylor <jameson@ionic.io>
2022-03-23 20:00:47 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'picker-v6-issue',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
