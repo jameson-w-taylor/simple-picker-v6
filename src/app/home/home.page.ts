2cce6038277dc32c4a3b768373c8bed06d0f6903
Jameson Taylor <jameson@ionic.io>
2022-03-23 20:18:31 -0400

    Add README;

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PickerController, PickerOptions } from '@ionic/angular';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
